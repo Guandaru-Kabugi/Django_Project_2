16ba790c94c982da994f822738ba8d23d030648d
Guandaru-Kabugi <kabugi96@gmail.com>
2024-06-29 10:52:22 +0300

    modifications to my food app

--- myfoodapp/food/urls.py
+++ myfoodapp/food/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('hello', views.index, name='index'),
+    path('', views.index, name='index'),
     path('mine',views.item, name='item'),
     path('january', views.january, name='January')
 ]
