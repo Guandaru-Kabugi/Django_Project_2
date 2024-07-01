1349baec7abea1abf5c1813e9875eacde4588c4f
Guandaru-Kabugi <kabugi96@gmail.com>
2024-07-01 12:32:03 +0300

    templates

--- myfoodapp/food/urls.py
+++ myfoodapp/food/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:item_id>/',views.detail,name='detail'),
     path('mine',views.item, name='item'),
     path('january', views.january, name='January')
 ]
